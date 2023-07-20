f0687af0a4cc804b6144c641e0ad47a12e10a66e
FranciscoJavierMejia <fjmejia@pichincha.com>
2023-07-20 08:54:41 -0500

    ajuste routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const MAIN = '/main';
+const MAIN = '/main/listado';
 
 const routes: Routes = [
       { path: '', redirectTo: MAIN, pathMatch: 'full' },
